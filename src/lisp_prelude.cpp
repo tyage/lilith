2a9467d045dbcedf00bacba33be81180f98b8593
Kugayama Nana <nonamea774@gmail.com>
2020-05-25 19:57:31 +0900

    lispでdefineしていく。

--- src/lisp_prelude.cpp
+++ src/lisp_prelude.cpp
@@ -1,13 +1,24 @@
 #include "lisp_prelude.hpp"
 
+#include <sstream>
+
+Value to_Lisp(char const* code) {
+  std::stringstream ss{code};
+  return read(ss);
+}
+
+Value operator""_lisp(char const* code, size_t) {
+  return to_Lisp(code);
+}
+
 Value prelude_lisp_defines(Value env) {
   std::array defines = {
-    list("define", "ans", 42_i),
-    list("define", "id", list("lambda", list("x"), "x")),
+    "(define id (lambda (x) x))",
+    "(define + (lambda (x y) (if (eq y 0) x (+ (succ x) (pred y)))))",
+    "(define x 42)",
   };
   for(auto v: defines) {
-    std::tie(std::ignore, env) = eval_define(v, env);
+    std::tie(std::ignore, env) = eval_define(to_Lisp(v), env);
   }
   return env;
 }
-
